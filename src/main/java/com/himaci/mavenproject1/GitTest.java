b5cf3e5ee9130473ea0b9b3c16aedd9532f9df73
junnotantra <junnotantra@gmail.com>
2014-02-22 22:03:40 +0700

    add

---
+++ src/main/java/com/himaci/mavenproject1/GitTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.himaci.mavenproject1;
+
+/**
+ *
+ * @author Junta
+ */
+public class GitTest {
+    private int X;
+    public GitTest() {
+        X=0;
+    }
+    public GitTest(int x){
+        X=x;
+    }
+    
+}
